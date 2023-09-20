3ab32cf8bd87c9de4e6ed839ed34e79ba8310a09
ricoloic <lorico@ebay.com>
2023-09-19 20:28:25 -0400

    feat: added new flocking (boids) sketch

--- public/js/utils/draw.js
+++ public/js/utils/draw.js
@@ -8,3 +8,12 @@ function fillCircle(ctx, x, y, radius) {
     ctx.arc(x, y, radius, 0, TWO_PI);
     ctx.fill();
 }
+
+function fillTriangle(ctx, x1, y1, x2, y2, x3, y3) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.lineTo(x3, y3);
+    ctx.closePath();
+    ctx.fill();
+}
