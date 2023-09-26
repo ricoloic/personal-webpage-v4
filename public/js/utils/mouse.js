5bb3c1bb3e835dda6f0c3869f8406a06e658b973
ricoloic <ricoloic4@gmail.com>
2023-09-26 17:36:25 -0400

    feat: added new sketches besier curve

--- public/js/utils/mouse.js
+++ public/js/utils/mouse.js
@@ -15,3 +15,23 @@ function mousePosition(canvas) {
 
     return position;
 }
+
+function startClick(canvas, callback) {
+    canvas.addEventListener("mousedown", function (event) {
+        const rect = canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        callback({ x, y });
+    });
+    canvas.addEventListener("touchstart", function (event) {
+        const rect = canvas.getBoundingClientRect();
+        const x = event.touches[0].pageX - rect.left;
+        const y = event.touches[0].pageY - rect.top;
+        callback({ x, y });
+    });
+}
+
+function stopClick(canvas, callback) {
+    canvas.addEventListener("mouseup", callback);
+    canvas.addEventListener("touchend", callback);
+}
